0f1758c5e29e9a9f901221e7ec0c23c08fd22437
Marek Klikar <makauer@icloud.com>
2024-11-03 10:40:18 +0100

    feat(menu) menu display support added for all menus

--- menu.py
+++ menu.py
@@ -20,3 +20,20 @@ class Menu:
             "rot47"
         ]
 
+    def get_choice(self, options: list) -> str:
+        return select(options, cursor=">>", cursor_style="cyan")
+
+    def main_menu_choice(self) -> str:
+        console.print("Wybierz opcję: ")
+
+        return self.get_choice(self.main_menu)
+
+    def source_menu_choice(self) -> str:
+        console.print("Skąd pobrać dane do operacji?")
+
+        return self.get_choice(self.source_menu)
+
+    def rot_menu_choice(self) -> str:
+        console.print("Podaj rodzaj szyfrowania:")
+
+        return self.get_choice(self.rot_menu)
