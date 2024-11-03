9adb458ef9acbb0903a9ae711c913cdfedcf9dec
Marek Klikar <makauer@icloud.com>
2024-11-03 10:42:13 +0100

    feat(buffer) create Buffer class with adding & getting_all

---
+++ buffer.py
@@ -0,0 +1,13 @@
+class Buffer:
+    def __init__(self):
+        self.buffer_list = []
+
+    def add_buffer(self, text_json: dict[str]) -> None:
+        self.buffer_list.append(text_json)
+
+    def get_all_buffer(self) -> [str, list]:
+        if len(self.buffer_list) < 1:
+            return "Nie przesłałeś jeszcze żadnych danych do szyfrowania."
+
+        return self.buffer_list
+
