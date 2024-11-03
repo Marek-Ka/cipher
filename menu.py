f910183706b1282a73db3a35730ccf9521123150
Marek Klikar <makauer@icloud.com>
2024-11-03 10:36:25 +0100

    feat(menu) create Menu with main_manu and install beaupy

---
+++ menu.py
@@ -0,0 +1,13 @@
+from beaupy import *
+
+
+class Menu:
+    def __init__(self):
+        self.main_menu = [
+            "Zaszyfruj dane",
+            "Odszyfruj dane",
+            "Zapisz do pliku",
+            "Odczytaj z pliku",
+            "Pokaż wykaz dzisiejszych operacji",
+            "Zakończenie programu"
+        ]
